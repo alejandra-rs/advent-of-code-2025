bf4596f79f5484be6dbbb0fc84b6adeecba70c44
Pooul14 <alejandra.rodriguezsilva2000@gmail.com>
2025-12-07 10:45:54 +0000

    AoC 4a finished.

--- src/test/java/software/aoc/day04/a/ForkliftManagerTest.java
+++ src/test/java/software/aoc/day04/a/ForkliftManagerTest.java
@@ -2,6 +2,9 @@ package software.aoc.day04.a;
 
 import org.junit.Test;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class ForkliftManagerTest {
@@ -29,4 +32,10 @@ public class ForkliftManagerTest {
         assertThat(ForkliftManager.with(diagram).accesibleRollCount()).isEqualTo(13);
     }
 
+    @Test
+    public void reward() throws IOException {
+        try (InputStream inputStream = ForkliftManagerTest.class.getResourceAsStream("/day04-a/input_aoc4.txt")) {
+            assertThat(ForkliftManager.with(new String(inputStream.readAllBytes())).accesibleRollCount()).isEqualTo(1505);
+        }
+    }
 }
