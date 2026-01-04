9ae0163a5b1fea60d8d289d2c6bb36ce520f4278
Pooul14 <alejandra.rodriguezsilva2000@gmail.com>
2026-01-04 23:45:00 +0000

    Refactored 11a and 11b implemented

---
+++ src/test/java/software/aoc/day11/a/ReactorTest.java
@@ -0,0 +1,35 @@
+package software.aoc.day11.a;
+
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class ReactorTest {
+
+    static final String devices = """
+                              aaa: you hhh
+                              you: bbb ccc
+                              bbb: ddd eee
+                              ccc: ddd eee fff
+                              ddd: ggg
+                              eee: out
+                              fff: out
+                              ggg: out
+                              hhh: ccc fff iii
+                              iii: out
+                              """;
+
+    @Test
+    public void given_devices_should_count_number_of_paths_to_out() {
+        assertThat(Reactor.from(devices).nPathsToOut()).isEqualTo(5);
+    }
+
+    @Test
+    public void reward() throws IOException {
+        try (InputStream inputStream = ReactorTest.class.getResourceAsStream("/day11/input_aoc11.txt")) {
+            assertThat(Reactor.from(new String(inputStream.readAllBytes())).nPathsToOut())
+                    .isEqualTo(585);
+        }
+    }
+}
---
+++ src/test/java/software/aoc/day11/b/ReactorTest.java
@@ -0,0 +1,39 @@
+package software.aoc.day11.b;
+
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class ReactorTest {
+
+    static final String devices = """
+                                  svr: aaa bbb
+                                  aaa: fft
+                                  fft: ccc
+                                  bbb: tty
+                                  tty: ccc
+                                  ccc: ddd eee
+                                  ddd: hub
+                                  hub: fff
+                                  eee: dac
+                                  dac: fff
+                                  fff: ggg hhh
+                                  ggg: out
+                                  hhh: out
+                                  """;
+
+    @Test
+    public void given_devices_should_count_number_of_valid_paths_to_out() {
+        assertThat(Reactor.with(devices).nPathsToOut()).isEqualTo(2);
+    }
+
+    @Test
+    public void reward() throws IOException {
+        try (InputStream inputStream = ReactorTest.class.getResourceAsStream("/day11/input_aoc11.txt")) {
+            assertThat(Reactor.with(new String(inputStream.readAllBytes())).nPathsToOut())
+                    .isEqualTo(349322478796032L);
+        }
+    }
+
+}
